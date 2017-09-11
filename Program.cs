d4e02c447ba0225cf85fe3988a01904aa5b96c1b
jmike <jmike@DESKTOP-54L68HD>
2017-09-10 17:21:25 -0700

    A small program that pulls a random number and encrypts it based off of the offset in a list of said random number

--- Program.cs
+++ Program.cs
@@ -10,6 +10,57 @@ namespace PasswordEncryption
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Encrypt("Password"));
+        }
+
+        public static string Encrypt(string password)
+        {
+            string encryptedPassword = "";
+            int counter = 0;
+            int hash = 0;
+            int i = 0;
+            Random R = new Random();
+            int offset = R.Next(5, 31);
+
+            List<string> hashChars = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
+
+            for (; i <= 35; i++)
+            {
+                if (i > 25)
+                {
+
+                    hashChars.Add(counter.ToString());
+                    counter++;
+                }
+                else
+                {
+                    string upperCase = hashChars[i].ToString().ToUpper();
+                    hashChars.Add(upperCase);
+                }
+            }
+
+            i = 0;
+
+            for (; i < password.Length; i++)
+            {
+
+                if (hashChars.IndexOf(password[i].ToString()) + offset > hashChars.Count)
+                {
+                    hash += hashChars.IndexOf(password[i].ToString()) + offset;
+
+                    hash -= hashChars.Count;
+                }
+                else
+                {
+                    hash = hashChars.IndexOf(password[i].ToString()) + offset;
+                }
+
+                encryptedPassword += hashChars[hash];
+                hash = 0;
+            }
+
+            return encryptedPassword;
         }
     }
 }
